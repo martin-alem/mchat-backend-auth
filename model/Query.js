3bc8d374538e47a86f215e9d7becd299fd5875fb
martin-alem <alemajohmartin@gmail.com>
2021-07-23 11:08:16 -0400

    bug fix for deployment

--- model/Query.js
+++ model/Query.js
@@ -8,12 +8,12 @@ const Database = require(path.join(__dirname, "./Database"));
 const Logger = require(path.join(__dirname, "../utils/Logger"));
 
 const databaseConfig = {
-	host: process.env.D_HOST || "localhost",
-	user: process.env.D_USER || "root",
-	password: process.env.D_PASSWORD || "",
-	database: process.env.DATABASE || "mchat",
-	port: process.env.D_PORT || 3306,
-	sslmode: process.env.SSL_MODE
+	host: process.env.D_HOST || "mchat-do-user-9421788-0.b.db.ondigitalocean.com",
+	user: process.env.D_USER || "doadmin",
+	password: process.env.D_PASSWORD || "ucodws0mfey2k5f4",
+	database: process.env.DATABASE || "test_mchat",
+	port: process.env.D_PORT || 25060,
+	sslmode: process.env.SSL_MODE || "REQUIRED"
 };
 
 const connection = new Database(databaseConfig);
@@ -21,11 +21,11 @@ const connection = new Database(databaseConfig);
 class Query {
 
 	/**
-     * Inserts a specific record in the table
-     * @param {string} tableName 
-     * @param {object} value 
-     * @returns {promise} promise resolves with the query result.
-     */
+	 * Inserts a specific record in the table
+	 * @param {string} tableName 
+	 * @param {object} value 
+	 * @returns {promise} promise resolves with the query result.
+	 */
 	static insert(tableName, value) {
 		return new Promise((resolve, reject) => {
 
@@ -42,10 +42,10 @@ class Query {
 	}
 
 	/**
-     * Selects all records in th table
-     * @param {string} tableName name of the table 
-     * @returns {promise} promise resolves with the query result
-     */
+	 * Selects all records in th table
+	 * @param {string} tableName name of the table 
+	 * @returns {promise} promise resolves with the query result
+	 */
 	static selectAll(tableName) {
 
 		return new Promise((resolve, reject) => {
@@ -63,12 +63,12 @@ class Query {
 	}
 
 	/**
-     * Selects a specific record in th table
-     * @param {string} tableName name of the table
-     * @param {string} field name of table column
-     * @param {any} value value
-     * @returns {promise} promise resolves with the query result
-     */
+	 * Selects a specific record in th table
+	 * @param {string} tableName name of the table
+	 * @param {string} field name of table column
+	 * @param {any} value value
+	 * @returns {promise} promise resolves with the query result
+	 */
 	static selectOne(tableName, field, value) {
 
 		return new Promise((resolve, reject) => {
@@ -86,14 +86,14 @@ class Query {
 	}
 
 	/**
-     * Updates a specific record in th table
-     * @param {string} tableName name of the table
-     * @param {string} uField update column
-     * @param {string} uValue update value
-     * @param {string} field table column
-     * @param {string} value value
-     * @returns {promise} promise resolves with the query result
-     */
+	 * Updates a specific record in th table
+	 * @param {string} tableName name of the table
+	 * @param {string} uField update column
+	 * @param {string} uValue update value
+	 * @param {string} field table column
+	 * @param {string} value value
+	 * @returns {promise} promise resolves with the query result
+	 */
 	static updateOne(tableName, uField, uValue, field, value) {
 
 		return new Promise((resolve, reject) => {
@@ -111,13 +111,13 @@ class Query {
 	}
 
 	/**
-     * Updates multiple rows in th table
-     * @param {string} tableName table name
-     * @param {object} updateObject update object
-     * @param {string} uField field to update
-     * @param {string} uValue value used to update
-     * @returns {promise} promise that resolves with the updated id
-     */
+	 * Updates multiple rows in th table
+	 * @param {string} tableName table name
+	 * @param {object} updateObject update object
+	 * @param {string} uField field to update
+	 * @param {string} uValue value used to update
+	 * @returns {promise} promise that resolves with the updated id
+	 */
 	static updateMany(tableName, updateObject, uField, uValue) {
 
 		return new Promise((resolve, reject) => {
@@ -137,10 +137,10 @@ class Query {
 	}
 
 	/**
-     * Performs two operations and rollback if any failed.
-     * @param {Array} transactions array of transactions
-     * @returns {promise} promise that will be resolved to true
-     */
+	 * Performs two operations and rollback if any failed.
+	 * @param {Array} transactions array of transactions
+	 * @returns {promise} promise that will be resolved to true
+	 */
 	static performTransaction(transactions) {
 
 		return new Promise((resolve, reject) => {
